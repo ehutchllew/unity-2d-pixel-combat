5e58837373c0a05b8a9d8723c1ce88ee6361d115
Evan <ehutch7@gmail.com>
2024-11-28 21:02:13 +0000

    Adding swing down animation; about to do swing up.

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    private Animator myAnimator;
+    private PlayerControls playerControls;
+
+    private void Attack()
+    {
+        // Fire sword animation
+        myAnimator.SetTrigger("Attack");
+    }
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerControls.Combat.Attack.started += _ => Attack();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
