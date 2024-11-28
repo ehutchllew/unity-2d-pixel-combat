3004012c5368f96376277959a202b4ae56f19d1a
Evan <ehutch7@gmail.com>
2024-11-28 23:57:54 +0000

    Adding logic to rotate sword in player facing direction

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Sword : MonoBehaviour
 {
+    private ActiveWeapon activeWeapon;
     private Animator myAnimator;
+    private PlayerController playerController;
     private PlayerControls playerControls;
 
     private void Attack()
@@ -12,8 +15,26 @@ public class Sword : MonoBehaviour
     }
     private void Awake()
     {
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
+        playerController = GetComponentInParent<PlayerController>();
         playerControls = new PlayerControls();
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        Vector3 playerScreenLoc = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        if (mousePos.x < playerScreenLoc.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+
+        if (mousePos.x > playerScreenLoc.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     private void OnEnable()
@@ -29,6 +50,6 @@ public class Sword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        MouseFollowWithOffset();
     }
 }
